5268a9348d1bfd3d54730f2da7d3605d466c9e41
Mangesh2042 <mangesh2042@gmail.com>
2022-03-24 19:57:15 +0530

    issue in data

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 
 const FormInput = (probs) => {
 
-    const [userName,setUserName]=useState()
-    const [userAge,setUserAge]=useState()
-    const [userAddress,setUserAddress]=useState()
-    const [userDepartment ,setUserDepartment ]=useState()
-    const [userSalary,setUserSalary]=useState()
-    const [userMarital ,setUserMarital ]=useState()
+    // const [userName,setUserName]=useState()
+    // const [userAge,setUserAge]=useState()
+    // const [userAddress,setUserAddress]=useState()
+    // const [userDepartment ,setUserDepartment ]=useState()
+    // const [userSalary,setUserSalary]=useState()
+    // const [userMarital ,setUserMarital ]=useState()
   
+    const [formData,setFormData]=useState([])
+
+
+    const handdle=(e)=>{
+        const {name,value}=e.target;
+
+        setFormData((prev)=>({...prev, [name]:value}));
+    }
    
+    const {userName,userAge,userAddress,userDepartment,userSalary,userMarital}=formData
 
     const addToData=()=>
     {
@@ -22,40 +31,40 @@ const FormInput = (probs) => {
             status="NO" 
         }
         
-
+        
         var item={
-            name:userName,
-            age:userAge,
-            address:userAddress,
-            department:userDepartment,
-            salary:userSalary,
+            name: userName,
+            age: userAge,
+            address: userAddress,
+            department: userDepartment,
+            salary: userSalary,
             marital:status
         }
 
       probs.Data(item);
-
-     console.log(userMarital);
+      console.log(userMarital);
         
        
     }
 
   return (
     <div>
-        <input type="name" placeholder='userName' value={userName} onChange={(e)=>setUserName(e.target.value)}/>
+        <input type="name" onChange={handdle} placeholder='userName' value={userName} name="userName"/>
         <br />
-        <input type="name" placeholder='userAge' value={userAge} onChange={(e)=>setUserAge(e.target.value)}/>
+        <input type="name" onChange={handdle}  placeholder='userAge' value={userAge}  name="userAge" />
         <br />
-        <input type="name" placeholder='userAddress' value={userAddress} onChange={(e)=>setUserAddress(e.target.value)}/>
+        <input type="name" onChange={handdle}  placeholder='userAddress' value={userAddress}  name="userAddress"/>
         <br />
-        <select value={userDepartment} onChange={(e)=>setUserDepartment(e.target.value)} >
+        <select value={userDepartment} name="userDepartment"  onChange={handdle}>
                  <option value="designer">designer</option>
                 <option value="testing">testing</option>
         </select>
         {/* <input type="name" placeholder='userDepartment' value={userDepartment} onChange={(e)=>setUserDepartment(e.target.value)}/> */}
         <br />
-        <input type="name" placeholder='userSalary' value={userSalary} onChange={(e)=>setUserSalary(e.target.value)}/>
+        <input type="name" placeholder='userSalary' onChange={handdle} value={userSalary}  name="userSalary"/>
         <br />
-        <input type="checkbox" placeholder='userMarital' checked={userMarital} onChange={(e)=>setUserMarital(e.target.checked)}/>
+        <label>Marital Status</label>
+        <input type="checkbox" placeholder='userMarital' onChange={handdle} checked={userMarital}  name="userMarital"/>
         <br />
         <button onClick={addToData}>Submit</button>
     </div>
